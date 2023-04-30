aaa80485c932dd41abdd3d24054b70b696321383
Arkadiy <woodythps@gmail.com>
2023-04-30 18:48:19 +0300

    Summary
    add: react, button component, userinfo component

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { UserInfo } from "./components/UserInfo/UserInfo" 
 
-function App() {
+export const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <UserInfo username="Alex Xamo"/>
+      <UserInfo username="Alex Mimo"/>
+      <UserInfo username="Alex Domo"/>
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ my-app/src/components/Buttons/Button.tsx
@@ -0,0 +1,25 @@
+import { FC } from 'react';
+import './Button.scss';
+
+
+interface IButton {
+    content: string;
+    type: 'primary' | 'secondary' | 'secondary2';
+    isDisabled: boolean;
+    onClick: () => void;
+}
+
+export const Button: FC<IButton> = ({content, isDisabled, onClick, type}) => {
+
+    const buttonClass = `button button--${type} ${isDisabled ? 'disabled' : 'active'}`
+
+    return (
+        <button 
+        className={buttonClass}
+        onClick={onClick} 
+        disabled ={isDisabled}
+        >
+            {content}
+        </button>
+    )
+}
